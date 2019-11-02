fcc8413ac3afea7053cb3e3d73b8863d3d00ef1b
Hunter Hodnett <hchodnett@gmail.com>
2019-11-01 21:39:46 -0500

    Generate brick inscriptions

--- Class1.cs
+++ Class1.cs
@@ -18,8 +18,11 @@ namespace Mason
         public string LyricLine2 { get; set; }
         public string LyricLine3 { get; set; }
         public string LyricLine4 { get; set; }
+        public int BrickEditionNumber { get; set; }
 
-        public BrickInscription(string artistName, string country, string lyricLine1, string lyricLine2, string lyricLine3, string lyricLine4, string donorName)
+        private static double TextHeight = 0.222;
+
+        public BrickInscription(string artistName, string country, string lyricLine1, string lyricLine2, string lyricLine3, string lyricLine4, string donorName, int brickEditionNumber)
         {
             ArtistName = artistName;
             Country = country;
@@ -28,9 +31,15 @@ namespace Mason
             LyricLine2 = lyricLine2;
             LyricLine3 = lyricLine3;
             LyricLine4 = lyricLine4;
+            BrickEditionNumber = brickEditionNumber;
         }
 
-        public static List<BrickInscription> ParseBrickInscriptionsFromCSV(string csvFilePath)
+        public override string ToString()
+        {
+            return "ArtistName: " + this.ArtistName + ", Country: " + this.Country + ", DonorName: " + this.DonorName;
+        }
+
+        public static List<BrickInscription> ParseBrickInscriptionsFromCSV(string csvFilePath, int brickEditionNumber)
         {
             List<BrickInscription> inscriptions = new List<BrickInscription>();
 
@@ -41,12 +50,10 @@ namespace Mason
 
                 string[] headerRow = parser.ReadFields();
                 int indexOfArtistNameColumn = BrickInscription.GetIndexOfArtistNameInHeaderRow(headerRow);
-
                 while (!parser.EndOfData)
                 {
                     //Process row
                     string[] fields = parser.ReadFields();
-
                     BrickInscription inscription = new BrickInscription(
                             fields[indexOfArtistNameColumn],
                             fields[indexOfArtistNameColumn + 1],
@@ -54,10 +61,13 @@ namespace Mason
                             fields[indexOfArtistNameColumn + 3],
                             fields[indexOfArtistNameColumn + 4],
                             fields[indexOfArtistNameColumn + 5],
-                            fields[indexOfArtistNameColumn + 6]
+                            fields[indexOfArtistNameColumn + 6],
+                            brickEditionNumber
                         );
 
                     inscriptions.Add(inscription);
+
+                    Console.WriteLine(inscription.ToString());
                 }
             }
 
@@ -68,18 +78,51 @@ namespace Mason
         {
             foreach (BrickInscription inscription in inscriptions)
             {
-                TextStyle style = new TextStyle("Another Typewriter.ttf");
+                List<EntityObject> entities = new List<EntityObject>();
 
-                MText text1 = new MText(Vector2.Zero, 10, 0, style);
-                // you can set manually the text value with all available formatting commands
-                text1.Value = inscription.DonorName;
+                MText donorName = BrickInscription.CreateMTextAtPosition(inscription.DonorName, 0.0, 0.0);
+                MText lylric1 = BrickInscription.CreateMTextAtPosition(inscription.LyricLine1, 0.0, 4.0);
+                MText lylric2 = BrickInscription.CreateMTextAtPosition(inscription.LyricLine2, 0.0, 3.0);
+                MText lylric3 = BrickInscription.CreateMTextAtPosition(inscription.LyricLine3, 0.0, 2.0);
+                MText lylric4 = BrickInscription.CreateMTextAtPosition(inscription.LyricLine4, 0.0, 1.0);
+                MText artistName = BrickInscription.CreateMTextAtPosition(inscription.ArtistName, 5.0, 0.0);
+                MText country = BrickInscription.CreateMTextAtPosition(inscription.Country, 5.0, 1.0);
+                MText brickEditionNumber = BrickInscription.CreateMTextAtPosition("#" + inscription.BrickEditionNumber.ToString(), 7.0, 4.0);
 
-                // both text1 and text2 should yield to the same result
-                DxfDocument dxf = new DxfDocument(DxfVersion.AutoCad2010);
-                dxf.AddEntity(text1);
+                entities.Add(donorName);
+                entities.Add(lylric1);
+                entities.Add(lylric2);
+                entities.Add(lylric3);
+                entities.Add(lylric4);
+                entities.Add(artistName);
+                entities.Add(country);
+                entities.Add(brickEditionNumber);
 
-                dxf.Save(BrickInscription.GenerateFileNameFromInscription(inscription));
+                string docName = BrickInscription.GenerateFileNameFromInscription(inscription);
+                BrickInscription.CreateDXFDocumentWithEntities(docName, entities);
             }
+        }
+
+        public static MText CreateMTextAtPosition(string textContent, double x, double y)
+        {
+            TextStyle style = new TextStyle("Another Typewriter.ttf");
+
+            MText text = new MText(new Vector2(x, y), BrickInscription.TextHeight, 0, style);
+            text.Value = textContent;
+
+            return text;
+        }
+
+        public static void CreateDXFDocumentWithEntities(string docName, List<EntityObject> entities)
+        {
+            DxfDocument dxf = new DxfDocument(DxfVersion.AutoCad2010);
+            
+            foreach (EntityObject entity in entities)
+            {
+                dxf.AddEntity(entity);
+            }
+
+            dxf.Save(docName);
         }
 
         private static int GetIndexOfArtistNameInHeaderRow(string[] headerRow)
@@ -89,7 +132,7 @@ namespace Mason
 
         private static string GenerateFileNameFromInscription(BrickInscription inscription)
         {
-            return inscription.DonorName + "_" + inscription.ArtistName + "_" + DateTime.Now.ToString("MM-dd-yyyy-h-mmtt") + ".dxf";
+            return inscription.DonorName + "_" + inscription.ArtistName + "_" + DateTime.Now.ToString("MM-dd-yyyy-h-mmtt") + new Random().Next().ToString() + ".dxf";
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -9,15 +9,72 @@ using netDxf;
 using netDxf.Entities;
 using netDxf.Header;
 using netDxf.Tables;
+using System.Windows.Forms;
 
 namespace Mason
 {
     class Program
     {
-        public static void Main()
+        [STAThread]
+        public static int Main(string[] args)
         {
-            List<BrickInscription> inscriptions = BrickInscription.ParseBrickInscriptionsFromCSV(@"C:\Users\Hunter\source\repos\Mason\orders.csv");
+            MessageBox.Show("Please select the CSV file containing the brick orders. Click OK to show file browser.", "Mason");
+            string csvFilePath = Program.GetFilePathOfCSV();
+
+            int brickEditionNumber = Program.ShowDialog(
+                "Please enter a starting index for the brick edition numbers, e.g. 106",
+                "Brick starting number"
+              );
+
+            List<BrickInscription> inscriptions = BrickInscription.ParseBrickInscriptionsFromCSV(csvFilePath, brickEditionNumber);
             BrickInscription.PrintInscriptionsToDXFFile(inscriptions);
+
+            MessageBox.Show("Brick inscription generation complete. You can find the brick inscriptions in the following folder: " + System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\generatedDXF", "Mason");
+
+            return 0;
+        }
+
+        public static string GetFilePathOfCSV()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Please select CSV file containing brick orders";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            else
+            {
+                throw new System.ArgumentException("File path to the CSV containing brick orders must be selected", "CSV");
+            }
+        }
+
+        public static int ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Width = 400 };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            if (prompt.ShowDialog() == DialogResult.OK)
+            {
+                return Int32.Parse(textBox.Text);
+            }
+            else
+            {
+                throw new System.ArgumentException("Argument must be specified and must be a number", caption);
+            }
         }
     }
 }
